7e124828841d3a2dbfad336ca718af8f22916580
RodrigoViolla <rodrigo_violla-alves@hotmail.com>
2018-05-09 00:15:22 -0300

    Classe Status adicionada

---
+++ Assets/Scripts/Status.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Status : MonoBehaviour {
+
+	private float vida = 100f;
+	private float mana = 100f;
+
+	public float Vida{
+		get{return vida;}
+	}
+	public float Mana{
+		get{return mana;}
+	}
+
+}
