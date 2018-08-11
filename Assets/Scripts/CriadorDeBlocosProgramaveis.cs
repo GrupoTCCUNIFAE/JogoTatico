c473ed40a0d7dfd53a4314c15f92db6829eb347c
RodrigoViolla <rodrigo_violla-alves@hotmail.com>
2018-08-11 10:49:56 -0300

    Cena padrao adicionada

---
+++ Assets/Scripts/CriadorDeBlocosProgramaveis.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CriadorDeBlocosProgramaveis : MonoBehaviour {
+
+	private GameObject blocoVazio;
+
+	// Use this for initialization
+	void Start () {
+		blocoVazio = gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (transform.childCount > 0) {
+			GameObject novoBloco = Instantiate (blocoVazio, Vector3.zero, Quaternion.identity);
+			novoBloco.transform.DetachChildren ();
+			novoBloco.transform.SetParent (transform.parent);
+			gameObject.GetComponent<CriadorDeBlocosProgramaveis> ().enabled = false;
+		}
+	}
+}
