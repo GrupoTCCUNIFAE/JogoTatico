4aa487a1c98441eaf80e1a61d25ec93b90895a19
reginaldobonaldi <reginaldobonaldi22@gmail.com>
2018-08-25 16:48:52 -0300

    Mapa

---
+++ Assets/Mapa/Map/Scripts/ControleDePosicao.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class ControleDePosicao : MonoBehaviour {
+
+	private Posicao posicao = new Posicao ("mapa_1", -168, -74);
+	public GameObject target;
+
+	void Start () {
+		//SceneManager.GetActiveScene()
+		if(posicao.Nome.Equals("mapa_1")){
+			target.transform.localPosition = new Vector3(posicao.PosicaoX, posicao.PosicaoY, 0);
+		}
+
+	}
+	
+
+
+}
