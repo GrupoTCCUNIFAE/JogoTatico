b51da4625a3519845d85e030a7b3f49e3751874f
RodrigoViolla <rodrigo_violla-alves@hotmail.com>
2018-08-27 18:06:41 -0300

    Cena de introducao adicionada

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card{
+
+	private Sprite imagem;
+	private string nome;
+	private string descricao;
+	private List<string> fraquesas;
+	private List<string> resistencias;
+	private List<string> imunidades;
+
+	public Card(Sprite imagem, string nome, string descricao, List<string> fraquesas, List<string> resistencias, List<string> imunidades){
+		this.imagem = imagem;
+		this. nome = nome;
+		this. descricao = descricao;
+		this. fraquesas = fraquesas;
+		this. resistencias = resistencias;
+		this. imunidades = imunidades;
+	}
+
+	public Sprite Imagem{
+		get{return imagem;}
+		set{ imagem = value; }
+	}
+	public string Nome{
+		get{return nome;}
+		set{nome = value;}
+	}
+	public string Descricao{
+		get{return descricao;}
+		set{descricao = value;}
+	}
+	public List<string> Fraquesas{
+		get{return fraquesas;}
+		set{fraquesas = value;}
+	}
+	public List<string> Resistencias{
+		get{return resistencias;}
+		set{resistencias = value;}
+	}
+	public List<string> Imunidades{
+		get{return imunidades;}
+		set{imunidades = value;}
+	}
+}
---
+++ Assets/Scripts/MonstronomiconUI.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MonstronomiconUI : MonoBehaviour {
+
+	public GameObject cardSlot;
+	public GameObject info;
+
+	private Inventario player;
+	private ScrollRect scrollRect;
+
+	void Start () {
+		Itens.CarregarCards ();
+		player = PlayerManager.instance.GetComponent<Inventario>();
+		scrollRect = GetComponent<ScrollRect> ();
+		AtualizarLivro ();
+		//transform.parent.parent.gameObject.SetActive (false);
+	}
+
+	void AtualizarLivro () {
+		for (int cnt = 0; cnt < scrollRect.content.transform.childCount; cnt++) {
+			Destroy(scrollRect.content.transform.GetChild (cnt).gameObject);
+		}
+
+		foreach(int idDaCarta in player.Cartas){
+			Card item = Itens.card [idDaCarta];
+			GameObject novoCard = Instantiate (cardSlot, Vector3.zero, Quaternion.identity);
+			novoCard.name = "Card do Inventario";
+			Image imagem = novoCard.transform.GetChild(0).GetComponent<Image> ();
+			Text texto = novoCard.transform.GetChild (1).GetComponent<Text> ();
+
+			novoCard.GetComponent<ItemUI> ().id = idDaCarta;
+			novoCard.GetComponent<ItemUI> ().info = info;
+			imagem.sprite = item.Imagem;
+			texto.text = item.Nome;
+
+			novoCard.transform.SetParent (scrollRect.content);
+		} 
+	}
+}
