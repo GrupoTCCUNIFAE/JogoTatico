61a23807f677e61f266eb3640853485dbf6e7c07
RodrigoViolla <rodrigo_violla-alves@hotmail.com>
2018-09-01 11:46:53 -0300

    Barra de mana sincronisada com a mana do jogador

--- Assets/Scripts/ControladorPlayerGUI.cs
+++ Assets/Scripts/ControladorPlayerGUI.cs
@@ -6,15 +6,17 @@ using UnityEngine.UI;
 public class ControladorPlayerGUI : MonoBehaviour {
 
 	public ControladorGeral statusDoPlayer;
+	public RectTransform barraDeMana;
 	public RectTransform barraDeVida;
 
 	private float total;
 
 	void Start(){
-		total = barraDeVida.anchorMax.y - barraDeVida.anchorMin.y;
+		total = barraDeMana.anchorMax.y - barraDeMana.anchorMin.y;
 	}
 
 	void Update () {
+		barraDeMana.anchorMax = new Vector2 (barraDeMana.anchorMax.x, (((statusDoPlayer.Mana)/100)*total)+barraDeMana.anchorMin.y);
 		barraDeVida.anchorMax = new Vector2 (barraDeVida.anchorMax.x, (((statusDoPlayer.Vida)/100)*total)+barraDeVida.anchorMin.y);
 	}
 }
